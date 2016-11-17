b0cfe3e5698d7873137327055796b757d9771277
bjarki10 <bjarki1234@gmail.com>
2016-11-17 17:19:56 +0000

    Adding gagnagrun and form

--- hopverk/hopverk/Form1.Designer.cs
+++ hopverk/hopverk/Form1.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.li_korfa = new System.Windows.Forms.ListView();
+            this.li_lager = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "bæta i korfu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Admin";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(756, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Check out";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // li_korfa
+            // 
+            this.li_korfa.FullRowSelect = true;
+            this.li_korfa.GridLines = true;
+            this.li_korfa.Location = new System.Drawing.Point(583, 34);
+            this.li_korfa.Name = "li_korfa";
+            this.li_korfa.Size = new System.Drawing.Size(434, 187);
+            this.li_korfa.TabIndex = 14;
+            this.li_korfa.UseCompatibleStateImageBehavior = false;
+            this.li_korfa.View = System.Windows.Forms.View.Details;
+            // 
+            // li_lager
+            // 
+            this.li_lager.FullRowSelect = true;
+            this.li_lager.GridLines = true;
+            this.li_lager.Location = new System.Drawing.Point(24, 34);
+            this.li_lager.Name = "li_lager";
+            this.li_lager.Size = new System.Drawing.Size(434, 187);
+            this.li_lager.TabIndex = 15;
+            this.li_lager.UseCompatibleStateImageBehavior = false;
+            this.li_lager.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 659);
+            this.Controls.Add(this.li_lager);
+            this.Controls.Add(this.li_korfa);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView li_korfa;
+        private System.Windows.Forms.ListView li_lager;
     }
 }
 
--- hopverk/hopverk/Form1.cs
+++ hopverk/hopverk/Form1.cs
@@ -7,14 +7,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace hopverk
 {
     public partial class Form1 : Form
     {
+
+
+        List<int> listi2 = new List<int>();
+
+        Random rnd = new Random();
+        List<int> listi = new List<int>();
+
+        List<string> linur = new List<string>();
+
+        string[] arr = new string[4];
+
+
+        Gagnagrunnur gagnagrunnur = new Gagnagrunnur();
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                gagnagrunnur.TengingVidGagnagrunn();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            li_lager.Clear();
+            li_lager.Columns.Add("Nafn", 200);
+            li_lager.Columns.Add("Voruflokkur", 120);
+            li_lager.Columns.Add("Verd", 60);
+            li_lager.Columns.Add("ALager", 60);
+
+            ListViewItem itm;
+
+            linur = gagnagrunnur.LesautSQLToflu();
+
+            try
+            {
+
+                foreach (string lin in linur)
+                {
+                    string[] linaUrLista = lin.Split(':');
+                    string Nafn = linaUrLista[0];
+                    string Voruflokkur = linaUrLista[1];
+                    string Verd = linaUrLista[2];
+                    string ALager = linaUrLista[3];
+
+                    arr[0] = Nafn;
+                    arr[1] = Voruflokkur;
+                    arr[2] = Verd;
+                    arr[3] = ALager;
+
+
+                    itm = new ListViewItem(arr);
+                    li_lager.Items.Add(itm);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            li_lager.Clear();
+            li_lager.Columns.Add("Nafn", 200);
+            li_lager.Columns.Add("Voruflokkur", 120);
+            li_lager.Columns.Add("Verd", 60);
+            li_lager.Columns.Add("ALager", 60);
+
+            ListViewItem itm;
+
+            linur = gagnagrunnur.LesautSQLToflu();
+
+            try
+            {
+
+                foreach (string lin in linur)
+                {
+                    string[] linaUrLista = lin.Split(':');
+                    string Nafn = linaUrLista[0];
+                    string Voruflokkur = linaUrLista[1];
+                    string Verd = linaUrLista[2];
+                    string ALager = linaUrLista[3];
+                    if (linaUrLista[0].ToLower().StartsWith(textBox1.Text))
+                    {
+                        arr[0] = Nafn;
+                        arr[1] = Voruflokkur;
+                        arr[2] = Verd;
+                        arr[3] = ALager;
+
+
+                        itm = new ListViewItem(arr);
+                        li_lager.Items.Add(itm);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
+
